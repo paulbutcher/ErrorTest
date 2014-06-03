5dce201138a68ff60416890047505607ceb743ff
Paul Butcher <paul@paulbutcher.com>
2014-06-03 11:59:18 +0100

    Slight tidyup

--- src-cljs/errortest/core.cljs
+++ src-cljs/errortest/core.cljs
@@ -1,7 +1,5 @@
 (ns errortest.core
   (:require [goog.debug.ErrorReporter :as reporter]))
 
-(enable-console-print!)
-
-(reporter/install "logError") 
+(reporter/install "/logError") 
 (.foobar js/window)
